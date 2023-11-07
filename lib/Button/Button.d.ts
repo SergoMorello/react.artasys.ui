3d135a66abc5e4117bd7d9e3ac9f3a80defc47b6
unknown <sergomorello@yandex.ru>
2023-11-07 12:14:28 +0300

    Поправил button

--- lib/Button/Button.d.ts
+++ lib/Button/Button.d.ts
@@ -1,8 +1,10 @@
 import React from "react";
+import { TSpinner } from "../Spinner";
 interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     wait?: boolean;
     classNameContainer?: string;
     styleContainer?: React.HTMLAttributes<HTMLDivElement>['style'];
+    spinnerColor?: TSpinner['color'];
 }
-declare const Button: ({ children, className, classNameContainer, styleContainer, wait, ...props }: IButton) => JSX.Element;
+declare const Button: ({ children, className, classNameContainer, styleContainer, spinnerColor, wait, ...props }: IButton) => JSX.Element;
 export default Button;
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import styles from "./style.module.css";
-import Spinner from "../Spinner";
+import Spinner,{
+	TSpinner
+} from "../Spinner";
 
 interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	wait?: boolean;
 	classNameContainer?: string;
 	styleContainer?: React.HTMLAttributes<HTMLDivElement>['style'];
+	spinnerColor?: TSpinner['color'];
 }
 
-const Button = ({children, className, classNameContainer, styleContainer, wait = false, ...props}: IButton) => {
+const Button = ({children, className, classNameContainer, styleContainer, spinnerColor = 'contrast', wait = false, ...props}: IButton) => {
 
 	return(<div className={styles['container'] + (classNameContainer ? ' ' + classNameContainer : '')} style={styleContainer}>
 		<button {...props} className={className}>{children}</button>
 		<div className={styles['wait-indicator'] + (wait ? ' ' + styles['active'] : '')}>
-			<Spinner size="small" color="contrast"/>
+			<Spinner size="small" color={spinnerColor}/>
 		</div>
 	</div>)
 };
