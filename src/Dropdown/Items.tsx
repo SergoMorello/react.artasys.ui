f476397d7f9282ec9b81582a66ad8c4a517acbf5
unknown <sergomorello@yandex.ru>
2024-11-18 15:11:51 +0300

    добавил слушатели на открытие и закрытие дропдаун

--- src/Dropdown/Items.tsx
+++ src/Dropdown/Items.tsx
@@ -14,11 +14,12 @@ const Items = ({items, isOpen, disabled}: IItems) => {
 		if (!listRef.current) return;
 		const screenWidth = window.innerWidth;
 		const element = listRef.current.getBoundingClientRect();
-		if (!isOpen) {
+		if (isOpen) {
 			listRef.current.ontransitionend = () => {
 				setHide(true);
 			};
 		}else{
+			listRef.current.ontransitionend = null;
 			setHide(false);
 		}
 		
