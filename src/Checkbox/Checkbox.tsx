dcc3abe0aabb29acbc74c2095892932255f5c0be
unknown <sergomorello@yandex.ru>
2026-02-06 15:15:04 +0300

    Добавил onChecked к Checkbox

--- src/Checkbox/Checkbox.tsx
+++ src/Checkbox/Checkbox.tsx
@@ -1,4 +1,7 @@
-import { forwardRef } from "react";
+import {
+	type ChangeEvent,
+	forwardRef
+} from "react";
 import Element,{
 	IElement
 } from "../Form/Element";
@@ -7,9 +10,19 @@ import { UIComponent } from "../ui-types";
 
 export interface ICheckbox extends UIComponent<IElement<HTMLInputElement>> {
 	type?: 'checkbox' | 'radio';
+	onChecked?: (status: boolean, value?: unknown) => void;
 }
 
-const Checkbox = forwardRef<HTMLInputElement, ICheckbox>(({type = 'checkbox', hiddenContainer = true, placeholder, ...props}, ref) => {
+const Checkbox = forwardRef<HTMLInputElement, ICheckbox>(({type = 'checkbox', hiddenContainer = true, placeholder, onChecked, onChange, ...props}, ref) => {
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		if (typeof onChange === 'function') {
+			onChange(event);
+		}
+		if (typeof onChecked === 'function') {
+			onChecked(event.target.checked, props.value);
+		}
+	};
 
 	return (
 		<Element {...props} hiddenContainer={hiddenContainer}>
@@ -19,6 +32,7 @@ const Checkbox = forwardRef<HTMLInputElement, ICheckbox>(({type = 'checkbox', hi
 							{...props}
 							className={'ui-checkbox' + (props.className ? ' ' + props.className : '')}
 							type={type}
+							onChange={handleChange}
 							ref={ref}
 						/>
 						<span className={'ui-checkbox-indicator ' + styles['indicator']} />
